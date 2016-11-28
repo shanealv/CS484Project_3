dbc6768346084097e5ae8de0c92b75647734f202
Ben Churchill <bencbchurchill@gmail.com>
2016-11-27 22:22:14 -0700

    minor changes

--- Network/src/Dispatcher.cpp
+++ Network/src/Dispatcher.cpp
@@ -2,27 +2,27 @@
 
 int Dispatcher::CurrentTime = 0;
 
-static void Dispatcher::QueuePacketCreation(int nodeId, int destinationId)
+void Dispatcher::QueuePacketCreation(int nodeId, int destinationId)
 {
 
 }
 
-static void Dispatcher::QueuePacketProcessing(int nodeId, std::shared_ptr<Packet> packet, int delay)
+void Dispatcher::QueuePacketProcessing(int nodeId, std::shared_ptr<Packet> packet, int delay)
 {
 
 }
 
-static void Dispatcher::QueuePacketUpload(int nodeId, int linkId, std::shared_ptr<Packet> packet, int delay)
+void Dispatcher::QueuePacketUpload(int nodeId, int linkId, std::shared_ptr<Packet> packet, int delay)
 {
 
 }
 
-static void Dispatcher::QueuePacketDownload(int linkId, std::shared_ptr<Packet> packet, int delay)
+void Dispatcher::QueuePacketDownload(int linkId, std::shared_ptr<Packet> packet, int delay)
 {
 
 }
 
-static int Dispatcher::GetCurrentTime()
+int Dispatcher::GetCurrentTime()
 {
 	return Dispatcher::CurrentTime;
 }
