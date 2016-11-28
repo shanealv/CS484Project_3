dd59f4c9e315839de8ba641c6bfc429dae6838b7
shanealv <shane.alv@gmail.com>
2016-11-28 14:42:05 -0700

    Ben added stuff without me knowing

--- Network/src/Dispatcher.cpp
+++ Network/src/Dispatcher.cpp
@@ -14,16 +14,19 @@ void Dispatcher::QueuePacketCreation(int nodeId, int destinationId)
 void Dispatcher::QueuePacketProcessing(int nodeId, std::shared_ptr<Packet> packet, int delay)
 {
 	Job newJob(nodeId, -1, -1, packet, CurrentTime + delay, JobType::PacketProcessing);
+	Dispatcher::JobQueue.push(newJob);
 }
 
 void Dispatcher::QueuePacketUpload(int nodeId, int linkId, std::shared_ptr<Packet> packet, int delay)
 {
 	Job newJob(nodeId, -1, linkId, packet, CurrentTime + delay, JobType::PacketUpload);
+	Dispatcher::JobQueue.push(newJob);
 }
 
 void Dispatcher::QueuePacketDownload(int linkId, std::shared_ptr<Packet> packet, int delay)
 {
 	Job newJob(-1, -1, linkId, packet, CurrentTime + delay, JobType::PacketDownload);
+	Dispatcher::JobQueue.push(newJob);
 }
 
 int Dispatcher::GetCurrentTime()
