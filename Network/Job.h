beced54d9d4471998fdb9faa611daf78b5d6a641
Ben Churchill <bencbchurchill@gmail.com>
2016-11-27 22:05:44 -0700

    created Job class

---
+++ Network/Job.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <memory>
+
+enum class JobType
+{
+	Processing,
+	PacketUpload,
+	PacketDownload
+};
+
+class Job
+{
+private:
+	int nodeId;
+	int linkId;
+	std::shared_ptr<Packet> packet;
+	double delay;
+	JobType job;
+public:
+	Job();
+	Job(int nid, int lid, std::shared_ptr<Packet> p, double d, JobType jt);
+	
+	int GetNodeId();
+	int GetLinkID();
+	std::shared_ptr<Packet> getPacket();
+	double GetDelay();
+	JobType GetType();
+	
+	void SetNodeId(int nid);
+	void SetLinkId(int lid);
+	void SetPacket(std::shared_ptr<Packet> p);
+	void SetDelay(double d);
+	void SetJobType(JobType jt);
+};
